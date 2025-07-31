923e5be874a0e59800d7fa33e96447bb506e0cf9
Meow <abodkaramma77@gmail.com>
2025-07-31 11:10:21 +0300

    version 1

---
+++ backend/generateSecretKey.py
@@ -0,0 +1,10 @@
+import os
+import secrets
+secret_key = secrets.token_hex(32)
+env_path = os.path.join(os.path.dirname(__file__), ".env")
+
+
+with open(env_path, "w") as f:
+    f.write(f"SECRET_KEY={secret_key}\n")
+
+print(f"✅ .env file created with SECRET_KEY:\n{secret_key}")
---
+++ front-end/static/js/login.js
@@ -0,0 +1,77 @@
+const $button = document.querySelector("button");
+const $email = document.querySelector("input[name='email']");
+const $password = document.querySelector("input[name='password']");
+const $errorDiv = document.getElementById("error-box");
+const $errorParagraph = document.querySelector("#error-box p");
+
+$button.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const email = $email.value.trim().toLowerCase();
+  const password = $password.value.trim();
+
+  if (!email.includes("gmail")) {
+    showError("Only Gmail addresses are allowed");
+    return;
+  }
+
+  if (password.length < 8) {
+    showError("Password too short (min 8 chars)");
+    return;
+  }
+
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const result = await response.json();
+
+    if (result.status === "success") {
+      alert("✅ Logged in successfully");
+      window.location.href = "/";
+    } else {
+      showError(result.message);
+    }
+  } catch (err) {
+    console.error("❌ Error:", err);
+    showError("Server error. Try again later.");
+  }
+});
+
+function showError(msg) {
+  $errorParagraph.textContent = msg;
+  $errorDiv.classList.remove("hidden");
+  setTimeout(() => $errorDiv.classList.add("hidden"), 4500);
+}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: window.GOOGLE_CLIENT_ID, // Flask will inject this
+    callback: handleCredentialResponse,
+  });
+
+  google.accounts.id.renderButton(document.getElementById("googleSignInDiv"), {
+    theme: "outline",
+    size: "large",
+    width: 300,
+  });
+};
+
+function handleCredentialResponse(response) {
+  fetch("/google-callback", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      credential: response.credential,
+    }),
+  }).then((res) => {
+    if (res.status != "fail") {
+      console.log(res.status, res.status == "fail");
+      window.location.href = res.url;
+    } else {
+      console.log("Login failed");
+    }
+  });
+}
